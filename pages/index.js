635745d14142e493d0c5907d8f9bf76e0b5036aa
Ben <ben.dalton@madetech.com>
2021-01-12 12:11:36 +0000

    add some start page styling

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+
   return (
     <Layout>
       <div className={styles.container}>
@@ -14,24 +15,28 @@ export default function Home() {
         <main className={styles.main}>
           <h1 className={styles.title}>Here to help</h1>
 
-          <p className={styles.description}>
-            Search for residents
-            <a href="/residents"> GO</a>
-          </p>
+          <table class={[styles.govukTable, styles.lbhTable].join(' ')}>
+            <tbody class="govuk-table__body">
 
+            <p className={styles.description}>
+            Search for residents
+              <a href="/residents" className={[styles.govukButton,styles.lbhButton].join(' ')}> GO</a>
+            </p>
           <hr />
 
           <p className={styles.description}>
             View callback list
-            <a href="/callback-list"> GO</a>
+            <a href="/callback-list" className={[styles.govukButton,styles.lbhButton].join(' ')}> GO</a>
           </p>
 
           <hr />
 
           <p className={styles.description}>
             Assign calls
-            <a href="/assign-calls"> GO</a>
+            <a href="/assign-calls" className={[styles.govukButton,styles.lbhButton].join(' ')}> GO</a>
           </p>
+          </tbody>
+          </table>
         </main>
 
         <footer className={styles.footer}></footer>
