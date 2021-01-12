ec87d291086206421333747724ffb30b32707d4a
duslerke <liudvikast@gmail.com>
2021-01-12 16:37:46 +0000

    Residents page - formatting changes (Prettier)

--- pages/residents.js
+++ pages/residents.js
@@ -1,72 +1,72 @@
 import { useRouter } from "next/router";
-import Layout from '../components/layout';
+import Layout from "../components/layout";
 
 export default function residents() {
   const router = useRouter();
   return (
     <Layout>
-    <div>
-      <a href="#" onClick={() => router.back()}>
-        Back
-      </a>
-      <h2 class="govuk-heading-xl govuk-!-margin-top-7">Resident lookup</h2>
-      <p class="govuk-body">
-        Search for resident by postcode <strong>or</strong> name to see if weve
-        helped them before.
-      </p>
-      <form action="/listresident">
-        <div class="govuk-grid-row row-margin-top-m">
-          <div class="govuk-grid-column-one-third">
-            <label class="govuk-label" for="postcode">
-              Postcode
-            </label>
-            <div class="govuk-form-group lbh-form-group">
-              <input
-                class="govuk-input govuk-input--width-10 lbh-input"
-                id="postcode"
-                name="postcode"
-                type="text"
-              />
+      <div>
+        <a href="#" onClick={() => router.back()}>
+          Back
+        </a>
+        <h2 class="govuk-heading-xl govuk-!-margin-top-7">Resident lookup</h2>
+        <p class="govuk-body">
+          Search for resident by postcode <strong>or</strong> name to see if
+          weve helped them before.
+        </p>
+        <form action="/listresident">
+          <div class="govuk-grid-row row-margin-top-m">
+            <div class="govuk-grid-column-one-third">
+              <label class="govuk-label" for="postcode">
+                Postcode
+              </label>
+              <div class="govuk-form-group lbh-form-group">
+                <input
+                  class="govuk-input govuk-input--width-10 lbh-input"
+                  id="postcode"
+                  name="postcode"
+                  type="text"
+                />
+              </div>
+            </div>
+
+            <div class="govuk-grid-column-one-third">
+              <label class="govuk-label" for="firstName">
+                First name
+              </label>
+              <div class="govuk-form-group lbh-form-group">
+                <input
+                  class="govuk-input govuk-input--width-12 lbh-input"
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                />
+              </div>
+            </div>
+
+            <div class="govuk-grid-column-one-third">
+              <label class="govuk-label" for="lastName">
+                Last name
+              </label>
+
+              <div class="govuk-form-group lbh-form-group">
+                <input
+                  class="govuk-input govuk-input--width-12 lbh-input"
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                />
+              </div>
             </div>
           </div>
-
-          <div class="govuk-grid-column-one-third">
-            <label class="govuk-label" for="firstName">
-              First name
-            </label>
-            <div class="govuk-form-group lbh-form-group">
-              <input
-                class="govuk-input govuk-input--width-12 lbh-input"
-                id="firstName"
-                name="firstName"
-                type="text"
-              />
-            </div>
-          </div>
-
-          <div class="govuk-grid-column-one-third">
-            <label class="govuk-label" for="lastName">
-              Last name
-            </label>
-
-            <div class="govuk-form-group lbh-form-group">
-              <input
-                class="govuk-input govuk-input--width-12 lbh-input"
-                id="lastName"
-                name="lastName"
-                type="text"
-              />
-            </div>
-          </div>
-        </div>
-        <button
-          class="govuk-button lbh-button js-cta-btn"
-          data-module="govuk-button"
-        >
-          Search
-        </button>
-      </form>
-    </div>
+          <button
+            class="govuk-button lbh-button js-cta-btn"
+            data-module="govuk-button"
+          >
+            Search
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 }
