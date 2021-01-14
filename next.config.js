40623b59f8ee66ed3d06a7e68a4307c28ea06616
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2021-01-14 22:18:23 +0000

    Add next config file for not running tests and exporting to build/_next

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    distDir: 'build/_next',
+    target: 'server',
+    webpack: (config, { webpack, isServer }) => {
+        config.plugins.push(new webpack.IgnorePlugin(/.*\.test\.ts$/));
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+            config.node = {
+                fs: 'empty',
+            };
+        }
+
+        return config;
+    },
+};
