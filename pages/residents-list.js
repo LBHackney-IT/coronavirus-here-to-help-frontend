b34663e3ab8f594db494f58cf06531c5e3a9c015
Ben <ben.dalton@madetech.com>
2021-01-14 12:44:24 +0000

    create ResidentList component to hold search results for residentslist page

--- pages/residents-list.js
+++ pages/residents-list.js
@@ -2,9 +2,33 @@ import React from 'react';
 import { useRouter } from "next/router";
 import { Button } from '../components/Form/Button/Button';
 import Layout from '../components/layout';
+import { useState } from 'react';
+import ResidentList from '../components/ResidentsList/ResidentList';
 
-export default function ResidentsList() {
+export default function ResidentsList({ searchPostcode }) {
     const router = useRouter();
+
+    const dummyResidentsList = [
+        {
+          first_name: "Bob",
+          last_name: "Hoskins",
+          address_first_line: "Flat 20 Caliban House",
+          address_second_line: "Frankfurt Street",
+          postcode: "N4 5JQ",
+          date_of_birth: '01/10/84',
+        },
+        {
+          first_name: "Mary",
+          last_name: "Johnson",
+          address_first_line: "Markmanor Ave 15",
+          address_second_line: "Url Street",
+          postcode: "E4 7NK",
+          date_of_birth: '09/03/54',
+        },
+      ];
+    
+    const [residents, setResidents] = useState(dummyResidentsList);
+
     return (
         <Layout>
         <div>
@@ -12,7 +36,7 @@ export default function ResidentsList() {
             <br />
             <h1 class="govuk-heading-xl" style={{marginBottom: '0.4em'}}>Search results</h1>
             <h2 class="govuk-heading-m">Help requests matching postcode: </h2>
-            <p class="govuk-body">Displaying 2 record(s)</p>
+            <p class="govuk-body">Displaying {residents.length} record(s)</p>
             <div class="govuk-grid-row">
                 <div class="govuk-grid-column-one-half">
                     <a href="/searchresident" role="button" draggable="false" class="govuk-button  govuk-button--secondary" data-module="govuk-button">
@@ -20,47 +44,12 @@ export default function ResidentsList() {
                     </a>
                 </div>
                 <div class="govuk-grid-column-one-half text-align-right">
-                    <a href="/editresident" role="button" draggable="false" class="govuk-button  lbh-button" data-module="govuk-button">
-                    Add new resident
+                    <a href="/editresident" role="button" draggable="false" class="govuk-button" data-module="govuk-button">
+                        Add new resident
                     </a>
                 </div>
             </div>
-            <table class="govuk-table  lbh-table">
-                <thead class="govuk-table__head">
-                    <tr class="govuk-table__row">
-                    <th scope="col" class="govuk-table__header">Name</th>
-                    <th scope="col" class="govuk-table__header">Address</th>
-                    <th scope="col" class="govuk-table__header">DOB</th>
-                    <th scope="col" class="govuk-table__header"></th>
-                    </tr>
-                </thead>
-                    <tbody class="govuk-table__body">
-                            <tr class="govuk-table__row">
-                                <td class="govuk-table__cell">
-                                    Bob Hoskins
-                                </td>
-                                <td class="govuk-table__cell">
-                                    THE HACKNEY SERVICE CENTRE, 1 HILLMAN STREET, E8 1DY
-                                </td>
-                                <td class="govuk-table__cell">01/01/1</td>
-                                <td class="govuk-table__cell">
-                                    <a data-testid="view-button"  href="/oneresident?empty=true" class="js-cta-btn" id="view-resident-3">View</a>
-                                </td>
-                            </tr>
-                            <tr class="govuk-table__row">
-                                <td class="govuk-table__cell">
-                                    Test Test
-                                </td>
-                                <td class="govuk-table__cell">
-                                    THE HACKNEY SERVICE CENTRE, 1 HILLMAN STREET, E8 1DY
-                                </td>
-                                <td class="govuk-table__cell">01/01/1</td>
-                                <td class="govuk-table__cell">
-                                    <a data-testid="view-button"  href="/oneresident?empty=true" class="js-cta-btn" id="view-resident-5">View</a>
-                                </td>
-                            </tr>
-                    </tbody>
-            </table>
+            <ResidentList residents={residents} />
         </div>
     </Layout>
     )
