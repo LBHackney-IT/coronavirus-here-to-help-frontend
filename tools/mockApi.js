57d057c67762b82c94a01ea8993062c88b90dbaf
duslerke <liudvikast@gmail.com>
2021-01-15 09:10:54 +0000

    Modified router render to extend Json-Server's embeding functionality
    Json-server supports relation nesting only for direct objects, so if I get my object via filter endpoint like this:
    \residents\:id\help_requests?_embed=case_notes
    then it won't be able to embed child entities to help_requests - it could only do it for the first mentioned entity - in this case residents.

--- tools/mockApi.js
+++ tools/mockApi.js
@@ -19,11 +19,34 @@ server.use(function (req, res, next) {
   next();
 });
 
+function getFilteredHelpRequestsWithCaseNotes(req, res) {
+  const url = req.originalUrl;
+  const isGET = req.method === "GET";
+  const urlPattern = /^\/residents?\/\d+\/help_requests\?_embed=case_notes$/i;
+  const urlMatches = url.match(urlPattern) !== null;
+
+  //If endpoint call matches, add case notes before returning response back
+  if (isGET && urlMatches) {
+    for (let i = 0; i < res.length; i++) {
+      const hreqId = res[i].id;
+      const assocCaseNotes = inMemDb.case_notes.filter(
+        (cn) => cn.help_requestId == hreqId
+      );
+      res[i].case_notes = assocCaseNotes;
+    }
+  }
+  return res;
+}
+
 router.render = (req, res) => {
+  // Json-Server doesn't have nesting support filtered objects, hence this
+  response = getFilteredHelpRequestsWithCaseNotes(req, res.locals.data);
+
   response = JSON.stringify(res.locals.data).replace(
     /(?<![\s\,\{\"])[Ii]d(?=\":)/g,
     "_id"
   );
+
   res.jsonp(JSON.parse(response));
 };
 
