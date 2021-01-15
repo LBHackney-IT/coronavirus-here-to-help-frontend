f03b06bcb742d1af1b1199edbf8ca3d0de657a4e
duslerke <liudvikast@gmail.com>
2021-01-15 22:10:16 +0000

    Added callbacks_list endpoint
    Left some question comments next to faked fake data (that's not even being pulled from fake db) - regarding currently unanswered back-end questions. Will remove these as they get resolved, as they serve as a good reminder of what's unclear.

--- tools/mockApi.js
+++ tools/mockApi.js
@@ -1,5 +1,7 @@
 const { dataGenerator } = require("./createMockData");
 const jsonServer = require("json-server");
+const randexp = require("randexp").randexp;
+const faker = require("faker");
 
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults({
@@ -53,7 +55,7 @@ const returnOnlyCreatedObjectsIdForPOSTRequests = (req, respData) =>
 
 router.render = (req, res) => {
   // Add different foreign key name format support
-  response = JSON.parse(
+  let response = JSON.parse(
     JSON.stringify(res.locals.data).replace(/(?<![\s\,\{\"])Id(?=\":)/g, "_id")
   );
   // Json-Server doesn't have nesting support filtered objects, hence this
@@ -71,7 +73,7 @@ server.use(middlewares);
 // that the resident_id should be taken from url
 server.post("/residents/:residentId/help_requests/", function (req, res, next) {
   req.url = "/help_requests";
-  let residentId = parseInt(req.params.residentId);
+  const residentId = parseInt(req.params.residentId);
   req.params = {};
   req.body["residentId"] = residentId;
   next();
@@ -81,12 +83,72 @@ server.post(
   "/residents/:residentId/help_requests/:help_requestId/calls",
   function (req, res, next) {
     req.url = "/help_request_calls";
-    let help_requestId = parseInt(req.params.residentId);
+    const help_requestId = parseInt(req.params.residentId);
     req.params = {};
     req.body["help_requestId"] = help_requestId;
     next();
   }
 );
+
+const isCallbackPredicate = (callbackRequired, InitialCallbackCompleted) =>
+  callbackRequired == true ||
+  callbackRequired == null ||
+  (InitialCallbackCompleted == false && callbackRequired == false);
+
+// Is the following check correct?
+const unsuccessfulCalls = (collection) =>
+  collection.filter((c) =>
+    /refused_to_engage|wrong_number/.test(c.call_outcome)
+  ).length;
+
+// because the url asks for param to be call_type, when in the db it's called help_needed
+const replaceObjectKey = (obj, currentKey, newKey) => {
+  obj[newKey] = obj[currentKey];
+  delete obj[currentKey];
+};
+
+server.get("/callback_list", function (req, res) {
+  const queryObj = req.query;
+  let helpRequests = inMemDb.help_requests; // starting point
+
+  replaceObjectKey(queryObj, "call_type", "help_needed");
+
+  for (param in queryObj)
+    helpRequests = helpRequests.filter(
+      (helpRequest) => helpRequest[param] == queryObj[param]
+    );
+  // == instead of === for potential fields that contain numbers - allows avoid parsing
+
+  helpRequests = helpRequests.filter((hr) =>
+    isCallbackPredicate(hr.callback_required, hr.initial_callback_completed)
+  );
+
+  helpRequests = helpRequests.map((helpRequest) => {
+    const resident = inMemDb.residents.find(
+      (r) => r.id == helpRequest.residentId
+    );
+    const calls = inMemDb.help_request_calls.filter(
+      (hrc) => hrc.help_requestId == helpRequest.id
+    );
+    const real_callback = {
+      resident_name: [resident.first_name, resident.last_name].join(" "),
+      id: resident.id,
+      address: [
+        resident.address_first_line,
+        resident.address_second_line,
+        resident.address_third_line,
+      ].join(", "),
+      requested_date: faker.date.soon(7), // where does this info is supposed to come from?!!! Need to add field to db schema?
+      type: helpRequest.help_needed,
+      unsuccessful_call_attempts: unsuccessfulCalls(calls),
+      follow_up_required: helpRequest.callback_required, // Is this correct assumption?
+      assigned_to: helpRequest.assigned_to,
+      rescheduled_at: randexp(/(0\d)|(1\d)|(2[0-3])[0-5]\d/), // Need to think about this one!!! Where is it stored? How do we set it on front-end?
+    };
+    return real_callback;
+  });
+  res.jsonp(helpRequests);
+});
 
 server.use(router);
 
