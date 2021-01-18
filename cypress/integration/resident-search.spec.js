7c7e462754dcbae09f6768bb158cb0c28c72932d
Ben <ben.dalton@madetech.com>
2021-01-18 14:52:01 +0000

    Add beforeEach hook with resident-search page url To avoid repetition of test set up in each test

--- cypress/integration/resident-search.spec.js
+++ cypress/integration/resident-search.spec.js
@@ -1,6 +1,9 @@
+beforeEach(() => {
+    cy.visit(`http://localhost:3000/resident-search`);
+});
+
 describe('View resident lookup page', () => {
     it('User can view the title', () => {
-        cy.visit(`http://localhost:3000/resident-search`);
         cy.get('h1').should('contain', 'Resident lookup');
     });
 });
@@ -8,16 +11,12 @@ describe('View resident lookup page', () => {
 describe('Test for user input', () => {
     context('Postcode input field', () => {
         it('Checks that a postcode can be entered', () => {
-            cy.visit(`http://localhost:3000/resident-search`);
-
             cy.get('input[name="postcode"]').type("E7 0DE");
             cy.get('input[name="postcode"]').invoke('val').should('not.be.empty');
             
         });
 
         it('Checks that the postcode is entered in the correct format', () => {
-            cy.visit(`http://localhost:3000/resident-search`);
-
             cy.get('input[name="postcode"]').type("E7 0DE");
             cy.get('input[name="postcode"]').invoke('val').should('match', /^[a-zA-Z]{1,2}[0-9]{1,2}[a-zA-Z]?\s?[0-9][a-zA-Z]{1,2}/);
             cy.get('input[name="postcode"]').clear();
