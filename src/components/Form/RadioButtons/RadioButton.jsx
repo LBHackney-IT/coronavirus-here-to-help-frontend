b47465cf1c65783d1cde5e63cf5408228eefd496
Ben <ben.dalton@madetech.com>
2021-01-18 18:04:58 +0000

    create RadioButton component

---
+++ src/components/Form/RadioButtons/RadioButton.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function RadioButton({ radioButtonItems }) {
+    return (
+        radioButtonItems.map((radioButtonItem) => {
+            return (
+                <div class="govuk-radios govuk-radios--inline">
+                <div class="govuk-radios__item">
+                    <input
+                        class="govuk-radios__input"
+                        id={radioButtonItem}
+                        name="HelpNeeded"
+                        type="radio"
+                        value={radioButtonItem}
+                    />
+                    <label
+                        class="govuk-label govuk-radios__label"
+                        for="HelpNeeded"
+                        style={{marginBottom: "15px"}}
+                    >
+                        {radioButtonItem}
+                    </label>
+                </div>
+            </div>
+            )
+        })
+    )
+}
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,5 +2,6 @@ import Button from "./Button/Button";
 import Checkbox from "./Checkboxes/Checkbox";
 import Dropdown from './Dropdown/Dropdown';
 import SupportTable from '../SupportTable/SupportTable';
+import RadioButton from "./RadioButtons/RadioButton";
 
-export { Button, Checkbox, Dropdown, SupportTable };
+export { Button, Checkbox, Dropdown, SupportTable, RadioButton };
