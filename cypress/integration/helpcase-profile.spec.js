f200febe84ec6d02df319bdb5b1402eb48adbfe3
Ben <ben.dalton@madetech.com>
2021-01-19 11:51:32 +0000

    update url on helpcase-profile test file

--- cypress/integration/helpcase-profile.spec.js
+++ cypress/integration/helpcase-profile.spec.js
@@ -1,6 +1,6 @@
 describe('View helpcase profile page', () => {
     it('User can view the title', () => {
-        cy.visit(`http://localhost:3000/helpcase-profile`);
+        cy.visit(`http://localhost:3000/helpcase-profile/1`);
         cy.get('h1').should('not.be.empty');
     });
 });
