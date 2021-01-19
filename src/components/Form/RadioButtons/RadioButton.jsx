2778bd0326e336511e690b5a8379613ff8395621
Ben <ben.dalton@madetech.com>
2021-01-19 15:54:15 +0000

    update RadioButton component to take name prop

--- src/components/Form/RadioButtons/RadioButton.jsx
+++ src/components/Form/RadioButtons/RadioButton.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 
-export default function RadioButton({ radioButtonItems }) {
+export default function RadioButton({ radioButtonItems, name }) {
     return (
-        radioButtonItems.map((radioButtonItem) => {
-            return (
-                <div class="govuk-radios govuk-radios--inline">
-                <div class="govuk-radios__item">
-                    <input
-                        class="govuk-radios__input"
-                        id={radioButtonItem}
-                        name={radioButtonItem}
-                        type="radio"
-                        value={radioButtonItem}
-                    />
-                    <label
-                        class="govuk-label govuk-radios__label"
-                        for={radioButtonItem}
-                        style={{marginBottom: "15px"}}
-                    >
-                        {radioButtonItem}
-                    </label>
+        <div class="govuk-radios govuk-radios--inline">
+            {radioButtonItems.map((radioButtonItem) => {
+                return (
+                    <div class="govuk-radios__item">
+                        <input
+                            class="govuk-radios__input"
+                            id={radioButtonItem}
+                            name={name}
+                            type="radio"
+                            value={radioButtonItem}
+                        />
+                        <label
+                            class="govuk-label govuk-radios__label"
+                            for={radioButtonItem}
+                            style={{marginBottom: "15px"}}
+                        >
+                            {radioButtonItem}
+                        </label>
                 </div>
-            </div>
-            )
-        })
+                )
+            })}
+        </div>
     )
 }
