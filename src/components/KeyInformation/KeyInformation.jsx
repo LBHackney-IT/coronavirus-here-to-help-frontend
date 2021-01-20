349b014644192e212cc9be7470d15986f3e9eeca
Kat <katrina@madetech.com>
2021-01-20 17:22:59 +0000

    Change the logic behind showing/hiding key information

--- src/components/KeyInformation/KeyInformation.jsx
+++ src/components/KeyInformation/KeyInformation.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./KeyInformation.module.scss";
+import { useState } from "react";
 
 export default function KeyInformation({ resident }) {
+  const [hidden, setHidden] = useState(false);
   const numbers = resident.ContactTelephoneNumber?.concat(
     resident.ContactMobileNumber
   );
+  const setHiddenValue = () => {
+    setHidden(!hidden);
+  };
+
   return (
     <div>
       <div className={styles["key-information-box"]}>
@@ -16,26 +22,27 @@ export default function KeyInformation({ resident }) {
             </a>
           </h3>
         </div>
-        {numbers?.map((number, index) => {
-          return (
-            <p data-testid={`key-information_phone-number_${index}`}>
-              Phone {index + 1}: {number}
-            </p>
-          );
-        })}
-        <br />
-        <h3 class="govuk-heading-s">Address</h3>
-        <p data-testid="key-information_resident-address">
-          {resident.AddressFirstLine}
+        <div hidden={hidden}>
+          {numbers?.map((number, index) => {
+            return (
+              <p data-testid={`key-information_phone-number_${index}`}>
+                Phone {index + 1}: {number}
+              </p>
+            );
+          })}
           <br />
-          {resident.AddressSecondLine}
+          <h3 class="govuk-heading-s">Address</h3>
+          <p data-testid="key-information_resident-address">
+            {resident.AddressFirstLine}
+            <br />
+            {resident.AddressSecondLine}
+            <br />
+            {resident.AddressThirdLine}
+            <br />
+            {resident.PostCode}
+          </p>
           <br />
-          {resident.AddressThirdLine}
-          <br />
-          {resident.PostCode}
-        </p>
-        <br />
-        {/* <h3 class="govuk-heading-s">Alternate contact:</h3>
+          {/* <h3 class="govuk-heading-s">Alternate contact:</h3>
         <p>
           Test Test
           <br />
@@ -45,34 +52,36 @@ export default function KeyInformation({ resident }) {
           <br />
           Phone 2: 071839
         </p> */}
+        </div>
       </div>
 
       <div className={styles["key-information-box"]}>
         <h3 class="govuk-heading-s">Key information</h3>
-        <p>
-          CEV
+        <div hidden={hidden}>
+          <p>
+            CEV
+            <br />
+            Positive Covid test at 2 Dec 2020
+            <br />
+            CEV Contact Tracing ID: SL 99 88 12 A
+          </p>
           <br />
-          Positive Covid test at 2 Dec 2020
+          <div>
+            <h3 className="govuk-heading-s">
+              Key notes:
+              <a href="/editresident#down" className={styles["edit-link-icon"]}>
+                ✎
+              </a>
+            </h3>
+          </div>
           <br />
-          CEV Contact Tracing ID: SL 99 88 12 A
-        </p>
-        <br />
-        <div>
-          <h3 className="govuk-heading-s">
-            Key notes:
-            <a href="/editresident#down" className={styles["edit-link-icon"]}>
-              ✎
-            </a>
-          </h3>
+          <p>{resident.KeyNotes}</p>
         </div>
-        <br />
-        <p>{resident.KeyNotes}</p>
       </div>
-      <a
-        href="#"
-        class="minimize"
-        onclick="document.getElementsByClassName('sticky-magic')[0].classList.toggle('hidden')"
-      ></a>
+      <a href="#" class="minimize" onClick={() => setHiddenValue()}>
+        {hidden && <p>Show</p>}
+        {!hidden && <p>Hide</p>}
+      </a>
     </div>
   );
 }
