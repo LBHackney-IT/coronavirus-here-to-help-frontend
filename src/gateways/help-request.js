b1b06ec87cafae69c518e8476894f779145537e4
duslerke <liudvikast@gmail.com>
2021-01-20 16:59:17 +0000

    Added putHelpRequest method to help request gateway

--- src/gateways/help-request.js
+++ src/gateways/help-request.js
@@ -1,10 +1,15 @@
-import { objectToQuery } from "../pages/api/utilityFuncs";
 import { DefaultGateway } from "../gateways/default-gateway";
 
 export class HelpRequestGateway extends DefaultGateway {
   async getHelpRequest(resident_id, request_id) {
     return await this.getFromUrl(
       `resident/${resident_id}/help_requests/${request_id}`
+    );
+  }
+  async putHelpRequest(resident_id, request_id, request_body) {
+    return await this.putToUrl(
+      `resident/${resident_id}/help_requests/${request_id}`,
+      request_body
     );
   }
 }
