b4305f9eddb4076c9a1becac3833947849ce51e7
duslerke <liudvikast@gmail.com>
2021-01-20 23:16:06 +0000

    Added Reassign Single Call page assigned call handler dropdown tests

--- cypress/integration/reassign-single-call.spec.js
+++ cypress/integration/reassign-single-call.spec.js
@@ -16,3 +16,22 @@ describe('Reassign single call page navigation', () => {
     });
 });
 
+describe('Reassign single call page displays and maps data correctly', () => {
+    it('Call handlers are retrieved and mapped to dropdown options', () => {
+        cy.visit(`http://localhost:3000/callback-list`);
+        cy.getBySel('callbacks-table').find('tbody > tr').eq(0).find(`td:has(a[title])`).find('a').click({force: true});
+        cy.wait(500);
+        cy.getBySel('call-handlers-dropdown').find('option').its('length').should('be.gte', 0)
+    });
+
+    it('Call handler dropdown\'s default value should match callback\'s assigned call handler', () => {
+        cy.visit(`http://localhost:3000/callback-list`);
+        cy.getBySel('call-handlers-dropdown').find('option').eq(4).invoke('val').then((callHandlerVal) => {
+            cy.getBySel('callbacks-table').find(`tbody > tr:has(a[title="${callHandlerVal}"])`)
+            .eq(0).find(`td:has(a[title])`).find('a').click({force: true});
+            cy.wait(500);
+            cy.getBySel('call-handlers-dropdown').invoke('val').should('eq', callHandlerVal);
+        })
+    });
+});
+
