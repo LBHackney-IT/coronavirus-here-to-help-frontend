57ac4c57b27e1cd1a8149c98be4dfde49b3d0e1d
duslerke <liudvikast@gmail.com>
2021-01-22 12:38:33 +0000

    Replace Key within an object function - will be needed when converting case

--- src/pages/api/utilityFuncs.js
+++ src/pages/api/utilityFuncs.js
@@ -4,6 +4,10 @@ const objectToQuery = (obj) =>
         .map((key) => key + '=' + obj[key])
         .join('&');
 
+const replaceObjectKey = (obj, currentKey, newKey) => {
+    obj[newKey] = obj[currentKey];
+    delete obj[currentKey];
+};
 
 const camelToPascal = (str) => str[0].toUpperCase() + str.substring(1);
 const pascalToCamel = (str) => str[0].toLowerCase() + str.substring(1);
