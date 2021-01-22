2718f8dbaa2e075eb7600adb2433da60f25f759b
duslerke <liudvikast@gmail.com>
2021-01-22 12:36:43 +0000

    Utility functions file formatting (autoformat)
    Separate commit so it would reduce the code noise in the actual commits.

--- src/pages/api/utilityFuncs.js
+++ src/pages/api/utilityFuncs.js
@@ -1,9 +1,10 @@
 const objectToQuery = (obj) =>
-  (Object.keys(obj).length > 0 ? "?" : "") +
-  Object.keys(obj)
-    .map((key) => key + "=" + obj[key])
-    .join("&");
+    (Object.keys(obj).length > 0 ? '?' : '') +
+    Object.keys(obj)
+        .map((key) => key + '=' + obj[key])
+        .join('&');
+
 
 module.exports = {
-  objectToQuery,
+    objectToQuery,
 };
