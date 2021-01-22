542901301652b4d5ed9190a768258a05f4365bcd
duslerke <liudvikast@gmail.com>
2021-01-22 12:39:30 +0000

    Added utility function for the parsing of the object tree's keys case

--- src/pages/api/utilityFuncs.js
+++ src/pages/api/utilityFuncs.js
@@ -12,6 +12,25 @@ const replaceObjectKey = (obj, currentKey, newKey) => {
 const camelToPascal = (str) => str[0].toUpperCase() + str.substring(1);
 const pascalToCamel = (str) => str[0].toLowerCase() + str.substring(1);
 
+function parseObjectTreeCase(item, convertCase) {
+    if (item === null || item === undefined) return;
+
+    let prototype = Object.getPrototypeOf(item);
+    let isObject = prototype === Object.prototype;
+    let isArray = prototype === Array.prototype;
+
+    if (!isObject && !isArray) return;
+
+    if (isObject) {
+        for (key in item) {
+            replaceObjectKey(item, key, convertCase(key));
+            parseObjectTreeCase(item[convertCase(key)], convertCase);
+        }
+    }
+
+    if (isArray) item.forEach((element) => parseObjectTreeCase(element, convertCase));
+}
+
 
 module.exports = {
     objectToQuery,
