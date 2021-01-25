97dbd7bc0e8879eb35a9513f85ce435d69577fc2
Kat <katrina@madetech.com>
2021-01-25 14:08:19 +0000

    Add case notes as part of the help request mock (to be confirmed) Display case notes data from the mock api

--- src/components/CaseNotes/CaseNotes.jsx
+++ src/components/CaseNotes/CaseNotes.jsx
@@ -1,60 +1,49 @@
-import React from "react";
-import { Button, Dropdown } from "../Form";
-import { useState } from "react";
+import React from 'react';
+import { Button, Dropdown } from '../Form';
+import { useState } from 'react';
 
-export default function CaseNotes() {
-	const fakeUsers = [
-		"Annalivia Ryan",
-		"Ben Dalton",
-		"Liudvikas T",
-		"Marten Wetterberg",
-	];
-	const supportTypes = [
-		"Help Request",
-		"CEV",
-		"Welfare",
-		"Shielding",
-		"Contact tracing",
-	];
+export default function CaseNotes({ helpRequests }) {
+    const supportTypes = ['Help Request', 'CEV', 'Welfare', 'Shielding', 'Contact Tracing'];
 
-	const [selectedSupportType, setSelectedSupportType] = useState("");
+    const [selectedSupportType, setSelectedSupportType] = useState();
 
-	const onChange = (value) => {
-		setSelectedSupportType(value);
-	};
+    const onChange = (value) => {
+        setSelectedSupportType(value);
+    };
 
-	return (
-		<div>
-			<h2 className="govuk-heading-l">Case notes</h2>
-			<Dropdown
-				onChange={(value) => onChange(value)}
-				dropdownItems={Object.values(supportTypes)}
-			/>
-			{fakeUsers.map((fakeUser, index) => {
-				return (
-					supportTypes[index] == selectedSupportType && (
-						<>
-							<div
-								style={{
-									backgroundColor: "rgba(220, 233, 213, 1)",
-									padding: "20px",
-									marginTop: "20px",
-								}}
-								id={supportTypes[index]}
-								className="filter"
-								data-type={supportTypes[index]}
-							>
-								<h4 className="govuk-heading-s" className="filter">
-									2020-01-0{Math.floor(Math.random() * 9)} by{" "}
-									{fakeUser}
-								</h4>
-								<p>{supportTypes[index]}: Random Text</p>
-							</div>
-							<hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible" />
-						</>
-					)
-				);
-			})}
-		</div>
-	);
+    return (
+        <div>
+            <h2 className="govuk-heading-l">Case notes</h2>
+            <Dropdown
+                onChange={(value) => onChange(value)}
+                dropdownItems={Object.values(supportTypes)}
+            />
+            {helpRequests.map((hr, i) => {
+                return hr.caseNotes?.map((note, x) => {
+                    return (
+                        (!selectedSupportType || selectedSupportType == hr.helpNeeded) && (
+                            <>
+                                <div
+                                    style={{
+                                        backgroundColor: 'rgba(220, 233, 213, 1)',
+                                        padding: '20px',
+                                        marginTop: '20px'
+                                    }}
+                                    id={`case-note-${note.id}`}
+                                    className="filter">
+                                    <h4 className="govuk-heading-s" className="filter">
+                                        {note.createdAt}
+                                    </h4>
+                                    <p>
+                                        {hr.helpNeeded}: {note.caseNote}
+                                    </p>
+                                </div>
+                                <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible" />
+                            </>
+                        )
+                    );
+                });
+            })}
+        </div>
+    );
 }
