bcb6650efa4b19472e437b48b62959f30c2621ba
Maysa <maysa@madetech.com>
2021-01-26 08:27:54 +0000

    fix gateway method name

--- src/gateways/help-request-call.js
+++ src/gateways/help-request-call.js
@@ -1,7 +1,7 @@
 import { DefaultGateway } from '../gateways/default-gateway';
 
 export class HelpRequestCallGateway extends DefaultGateway {
-    async postHelpRequest(helpRequestId, requestBody) {
+    async postHelpRequestCall(helpRequestId, requestBody) {
       return await this.postToUrl(`help-requests/${helpRequestId}/calls`, requestBody)
     }
 }
