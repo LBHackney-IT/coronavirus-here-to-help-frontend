0349715fa754f774a8e787a66c91fa3e130251e9
Kat <katrina@madetech.com>
2021-01-27 15:13:11 +0000

    Add search by postcode tests and rename searh result fixture

--- cypress/integration/resident-search.spec.js
+++ cypress/integration/resident-search.spec.js
@@ -3,8 +3,12 @@ beforeEach(() => {
 
     cy.intercept('/api/proxy/v4/residents?', (req) => {
         req.reply((res) => {
-            if (req.url.includes('Cydney') || req.url.includes('Nade')) {
-                res.send({ fixture: 'searchByName' });
+            if (
+                req.url.includes('Cydney') ||
+                req.url.includes('Nader') ||
+                req.url.includes('EW6')
+            ) {
+                res.send({ fixture: 'searchResult' });
             } else {
                 res.send({ statusCode: 200, body: [] });
             }
@@ -41,7 +45,7 @@ describe('As a call handler, I want to search for a resident', () => {
     context('When you search by last name', () => {
         it('displays an existing resident on the results page', () => {
             cy.visit(`http://localhost:3000/resident-search`);
-            cy.get('input[name="lastName"]').type('Nade');
+            cy.get('input[name="lastName"]').type('Nader');
             cy.get('.govuk-button').click();
             cy.get('[data-testid=residents-search-table]')
                 .find('tbody > tr')
@@ -59,16 +63,33 @@ describe('As a call handler, I want to search for a resident', () => {
             cy.contains('Displaying 0 record(s)');
         });
     });
-    // context("When you search by  postcode", () => {
-    //     it("displays an existing resident on the results page", () => {
-    //     })
-    //     it("does not display anything for a non existing resident on the results page", () => {
-    //     })
-    // })
-    // context("searching without any search options", () => {
-    //     it("validation errors and does not redirect you", () => {
-    //     })
-    // })
+
+    context('When you search by  postcode', () => {
+        it('displays an existing resident on the results page', () => {
+            cy.visit(`http://localhost:3000/resident-search`);
+            cy.get('input[name="postcode"]').type('EW6');
+            cy.get('.govuk-button').click();
+            cy.get('[data-testid=residents-search-table]')
+                .find('tbody > tr')
+                .its('length')
+                .should('be.gte', 1);
+            cy.get('[data-testid=residents-search-table]')
+                .contains('td', 'Cydney Nader')
+                .should('be.visible');
+            cy.contains('Displaying 1 record(s)');
+        });
+        it('does not display anything for a non existing resident on the results page', () => {
+            it('does not display anything for a non existing resident on the results page', () => {
+                cy.visit(`http://localhost:3000/resident-search`);
+                cy.get('input[name="postcode"]').type('rsdkjasbsf');
+                cy.get('.govuk-button').click();
+                cy.contains('Displaying 0 record(s)');
+            });
+        });
+    });
+    context('searching without any search options', () => {
+        it('validation errors and does not redirect you', () => {});
+    });
 
     context('From the start page', () => {
         it('User clicks the "Go" button to get to the Resident lookup page', () => {
