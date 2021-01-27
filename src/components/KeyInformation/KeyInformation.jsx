7dc4e3492e1b2286dd0873f0680492482a3647e6
Antony O'Neill <antony@ant-web.co.uk>
2021-01-27 15:17:49 +0000

    Prettier

--- src/components/KeyInformation/KeyInformation.jsx
+++ src/components/KeyInformation/KeyInformation.jsx
@@ -1,51 +1,54 @@
-import React from "react";
-import styles from "./KeyInformation.module.scss";
-import { useState } from "react";
+import React from 'react';
+import styles from './KeyInformation.module.scss';
+import { useState } from 'react';
 
 export default function KeyInformation({ resident }) {
-  const [hidden, setHidden] = useState(false);
-  const setHiddenValue = () => {
-    setHidden(!hidden);
-  };
+    const [hidden, setHidden] = useState(false);
+    const setHiddenValue = () => {
+        setHidden(!hidden);
+    };
 
-  return (
-    <div>
-      <div className={styles["key-information-box"]}>
+    return (
         <div>
-          <a href="#" onClick={() => setHiddenValue()}>
-            {hidden && <p>Show</p>}
-            {!hidden && <p>Hide</p>}
-          </a>
-          <h3 className="govuk-heading-s">
-            Contact details
-            <a
-              href={`/helpcase-profile/${resident.id}/editresident`}
-              className={styles["edit-link-icon"]}
-            >
-              ✎
-            </a>
-          </h3>
-        </div>
-        <div hidden={hidden}>
-              <p data-testid={`key-information_phone-number_1`} key={`key-information_phone-number_1`}>
-                Phone 1: {resident.contactTelephoneNumber}
-              </p>
-              <p data-testid={`key-information_phone-number_2`} key={`key-information_phone-number_2`}>
-                Phone 2: {resident.contactMobileNumber}
-              </p>
-          <br />
-          <h3 className="govuk-heading-s">Address</h3>
-          <p data-testid="key-information_resident-address">
-            {resident.addressFirstLine}
-            <br />
-            {resident.addressSecondLine}
-            <br />
-            {resident.addressThirdLine}
-            <br />
-            {resident.postCode}
-          </p>
-          <br />
-          {/* <h3 className="govuk-heading-s">Alternate contact:</h3>
+            <div className={styles['key-information-box']}>
+                <div>
+                    <a href="#" onClick={() => setHiddenValue()}>
+                        {hidden && <p>Show</p>}
+                        {!hidden && <p>Hide</p>}
+                    </a>
+                    <h3 className="govuk-heading-s">
+                        Contact details
+                        <a
+                            href={`/helpcase-profile/${resident.id}/editresident`}
+                            className={styles['edit-link-icon']}>
+                            ✎
+                        </a>
+                    </h3>
+                </div>
+                <div hidden={hidden}>
+                    <p
+                        data-testid={`key-information_phone-number_1`}
+                        key={`key-information_phone-number_1`}>
+                        Phone 1: {resident.contactTelephoneNumber}
+                    </p>
+                    <p
+                        data-testid={`key-information_phone-number_2`}
+                        key={`key-information_phone-number_2`}>
+                        Phone 2: {resident.contactMobileNumber}
+                    </p>
+                    <br />
+                    <h3 className="govuk-heading-s">Address</h3>
+                    <p data-testid="key-information_resident-address">
+                        {resident.addressFirstLine}
+                        <br />
+                        {resident.addressSecondLine}
+                        <br />
+                        {resident.addressThirdLine}
+                        <br />
+                        {resident.postCode}
+                    </p>
+                    <br />
+                    {/* <h3 className="govuk-heading-s">Alternate contact:</h3>
         <p>
           Test Test
           <br />
@@ -55,32 +58,34 @@ export default function KeyInformation({ resident }) {
           <br />
           Phone 2: 071839
         </p> */}
-        </div>
-      </div>
+                </div>
+            </div>
 
-      <div className={styles["key-information-box"]}>
-        <h3 className="govuk-heading-s">Key information</h3>
-        <div hidden={hidden}>
-          <p>
-            CEV
-            <br />
-            Positive Covid test at 2 Dec 2020
-            <br />
-            CEV Contact Tracing ID: SL 99 88 12 A
-          </p>
-          <br />
-          <div>
-            <h3 className="govuk-heading-s">
-              Key notes:
-              <a href={`/helpcase-profile/${resident.id}/editresident`} className={styles["edit-link-icon"]}>
-                ✎
-              </a>
-            </h3>
-          </div>
-          <br />
-          <p>{resident.keyNotes}</p>
+            <div className={styles['key-information-box']}>
+                <h3 className="govuk-heading-s">Key information</h3>
+                <div hidden={hidden}>
+                    <p>
+                        CEV
+                        <br />
+                        Positive Covid test at 2 Dec 2020
+                        <br />
+                        CEV Contact Tracing ID: SL 99 88 12 A
+                    </p>
+                    <br />
+                    <div>
+                        <h3 className="govuk-heading-s">
+                            Key notes:
+                            <a
+                                href={`/helpcase-profile/${resident.id}/editresident`}
+                                className={styles['edit-link-icon']}>
+                                ✎
+                            </a>
+                        </h3>
+                    </div>
+                    <br />
+                    <p>{resident.keyNotes}</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
