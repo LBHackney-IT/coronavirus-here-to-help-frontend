8259f4b7c834edbf6c781ec34740eb932a3d8c1e
Antony O'Neill <antony@ant-web.co.uk>
2021-01-27 13:57:47 +0000

    Passthrough params to component, useEffect to load
    By putting the load in a useeffect, we can bypass the refresh problem.
    
    Would be ideal to put this in the getServerSideProps, but the gateways
    currently cannot make server side proxy requests

--- src/pages/residents-list.js
+++ src/pages/residents-list.js
@@ -2,51 +2,65 @@ import React from 'react';
 import Link from 'next/link';
 import { Button } from '../components/Form';
 import Layout from '../components/layout';
-import { useState } from 'react';
 import ResidentList from '../components/ResidentsList/ResidentList';
-import { ResidentGateway } from "../gateways/resident";
+import { ResidentGateway } from '../gateways/resident';
+import { useEffect, useState } from 'react';
 
+export default function ResidentsList({ firstName, lastName, postcode }) {
+    const [residents, setResidents] = useState([]);
 
-export default function ResidentsList({res, postcode}) {
-    const [residents, setResidents] = useState(res);
+    const loadResidents = async () => {
+        const gateway = new ResidentGateway();
+        const residents = await gateway.getResidentsBySearchParams(
+            postcode.replace(/ /g, ''),
+            firstName,
+            lastName
+        );
+        setResidents(residents);
+    };
+
+    useEffect(() => {
+        loadResidents();
+    },[])
 
     return (
         <Layout>
-        <div>
-            <Link href="/resident-search">
-                <a href="#" className="govuk-back-link">Back</a>
-            </Link>
-            <br />
-            <h1 className="govuk-heading-xl" style={{marginBottom: '0.4em'}}>Search results</h1>
-            <h2 className="govuk-heading-m">Help requests matching postcode: {postcode}</h2>
-            <p className="govuk-body">Displaying {residents.length} record(s)</p>
-            <div className="govuk-grid-row">
-                <div className="govuk-grid-column-one-half">
-                    <Link href="/resident-search">
-                        <Button
-                            text="New search"
-                            addClass="govuk-button--secondary"
-                        />
-                    </Link>
-                </div>
-                <div className="govuk-grid-column-one-half text-align-right">
-                    {/* <Button
+            <div>
+                <Link href="/resident-search">
+                    <a href="#" className="govuk-back-link">
+                        Back
+                    </a>
+                </Link>
+                <br />
+                <h1 className="govuk-heading-xl" style={{ marginBottom: '0.4em' }}>
+                    Search results
+                </h1>
+                <p className="govuk-body">Displaying {residents.length} record(s)</p>
+                <div className="govuk-grid-row">
+                    <div className="govuk-grid-column-one-half">
+                        <Link href="/resident-search">
+                            <Button text="New search" addClass="govuk-button--secondary" />
+                        </Link>
+                    </div>
+                    <div className="govuk-grid-column-one-half text-align-right">
+                        {/* <Button
                             text="Add new resident"
                             style={{marginLeft: '15em'}}
                     /> */}
+                    </div>
+                    <ResidentList residents={residents} />
                 </div>
-                <ResidentList residents={residents} />
-            </div>
             </div>
         </Layout>
     );
 }
 
-ResidentsList.getInitialProps = async ({query}) => {
-    let postcode = query.postcode.replace(/ /g,'')
-    const firstName = query.firstName
-    const lastName = query.lastName 
-    const gateway = new ResidentGateway();
-    const res = await gateway.getResidentsBySearchParams(postcode, firstName, lastName);
-    return {res, postcode};
-  };
+export const getServerSideProps = async ({ query }) => {
+    return {
+        props: {
+            firstName: query.firstName || '',
+            lastName: query.lastName || '',
+            postcode: query.postcode || ''
+        }
+    };
+};
