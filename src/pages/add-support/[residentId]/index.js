a566085eca43d8aebb577074046d0d4579c67640
Maysa <maysa@madetech.com>
2021-01-27 12:30:52 +0000

    allow followup to be no when creating help request

--- src/pages/add-support/[residentId]/index.js
+++ src/pages/add-support/[residentId]/index.js
@@ -164,7 +164,7 @@ export default function addSupportPage({residentId}) {
 		}
 
 		if(callMade == false){
-			if(!callbackRequired || !helpNeeded){
+			if(callbackRequired=null || !helpNeeded){
 				setErrorsExist(true)
 		 	}else {
 			 try{
