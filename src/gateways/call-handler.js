799f381697b57fa3de7dfa718e34622d8420cc87
Kat <katrina@madetech.com>
2021-01-27 20:47:01 +0000

    Set call handler list based on stage

--- src/gateways/call-handler.js
+++ src/gateways/call-handler.js
@@ -2,7 +2,7 @@ import { DefaultGateway } from '../gateways/default-gateway';
 
 export class CallHandlerGateway extends DefaultGateway {
     async getCallHandler() {
-        if (process.env.APP_STAGE == 'dev') return ['Person A', 'Person B', 'Person C', 'Person D'];
+        if (process.env.NEXT_PUBLIC_APP_STAGE == 'dev') return ['Person A', 'Person B', 'Person C', 'Person D'];
         return process.env.NEXT_PUBLIC_CALL_HANDLERS
             ? process.env.NEXT_PUBLIC_CALL_HANDLERS.split(',')
             : [];
