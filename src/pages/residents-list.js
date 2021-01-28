1e09bfe79b2f69a49aa36fcab293b53f6cbe6f70
Maysa <maysa@madetech.com>
2021-01-28 09:08:58 +0000

    merge master

--- src/pages/residents-list.js
+++ src/pages/residents-list.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { Button } from '../components/Form';
 import Layout from '../components/layout';
