2e54b392d267dd1e235b99a9640be4afdc751f19
Ben <ben.dalton@madetech.com>
2021-01-28 16:26:29 +0000

    add resident gateway test for getResident fix mapping in resident gateway

--- src/gateways/resident.js
+++ src/gateways/resident.js
@@ -3,27 +3,21 @@ import { DefaultGateway } from '../gateways/default-gateway';
 const ToResident = (response) => {
     console.log(response)
     return {
-        firstName: response.FirstName,
         id: response.Id,
+        firstName: response.FirstName,
         lastName: response.LastName,
+        dobDay: response.DobDay,
+        dobMonth: response.DobMonth,
+        dobYear: response.DobYear,
+        contactTelephoneNumber: response.ContactTelephoneNumber,
+        contactMobileNumber: response.ContactMobileNumber,
+        emailAddress: response.EmailAddress,
         addressFirstLine: response.AddressFirstLine,
         addressSecondLine: response.AddressSecondLine,
         addressThirdLine: response.AddressThirdLine,
-        consentToShare: response.ConsentToShare,
-        contactMobileNumber: response.ContactMobileNumber,
-        contactTelephoneNumber: response.ContactTelephoneNumber,
-        dateOfBirth: response.DobDay +'/'+response.DobMonth+'/'+response.DobYear,
-        emailAddress: response.EmailAddress,
-        gpSurgeryDetails: response.GpSurgeryDetails,
-        isPharmacistAbleToDeliver: response.IsPharmacistAbleToDeliver,
-        nameAddressPharmacist: response.NameAddressPharmacist,
-        nhsNumber: response.NhsNumber,
-        numberOfChildrenUnder18: response.NumberOfChildrenUnder18,
         postCode: response.Postcode,
-        recordStatus: response.RecordStatus,
         uprn: response.Uprn,
-        ward: response.Ward,
-        keyNotes: response.KeyNotes,
+        nhsNumber: response.NhsNumber,
     };
 };
 
---
+++ src/gateways/resident.test.js
@@ -0,0 +1,71 @@
+import moxios from 'moxios';
+import { ResidentGateway } from './resident.js';
+
+describe('Case notes gateway', () => {
+    const resGateway = new ResidentGateway();
+
+    beforeEach(() => {
+        moxios.install();
+    });
+
+    afterEach(() => {
+        moxios.uninstall();
+    });
+
+    it('200 Success "/residents/:residentId"', async () => {
+        // arrange
+        const randomId = Math.floor(Math.random() * 20);
+        const mockAxiosResponse = 
+            {
+                Id: 1,
+                FirstName: "Russell",
+                LastName: "Botsford",
+                DobDay: "01",
+                DobMonth: "01",
+                DobYear: "1953",
+                ContactMobileNumber: "07084246866",
+                ContactTelephoneNumber: "02094644146",
+                EmailAddress: "Russell26@yahoo.com",
+                AddressFirstLine: "Flat 2",
+                AddressSecondLine: "9570 Gorczany Ford",
+                AddressThirdLine: "",
+                Postcode: "H6 3VA",
+                Uprn: "10007630135",
+                NhsNumber: "2106107057",
+            };
+
+        const expectedResponse = 
+            {
+                id: 1,
+                firstName: "Russell",
+                lastName: "Botsford",
+                dobDay: "01",
+                dobMonth: "01",
+                dobYear: "1953",
+                contactMobileNumber: "07084246866",
+                contactTelephoneNumber: "02094644146",
+                addressFirstLine: "Flat 2",
+                addressSecondLine: "9570 Gorczany Ford",
+                addressThirdLine: "",
+                emailAddress: "Russell26@yahoo.com",
+                postCode: "H6 3VA",
+                uprn: "10007630135",
+                nhsNumber: "2106107057",
+            };
+
+        const urlExp = new RegExp('v4/residents/' + randomId);
+
+        moxios.stubRequest(urlExp, {
+            status: 200,
+            response: mockAxiosResponse
+        });
+
+        // act
+        const gatewayResponse = await resGateway.getResident(randomId);
+
+        // assert
+        expect(gatewayResponse).toMatchObject(expectedResponse);
+    });
+});
+
+
