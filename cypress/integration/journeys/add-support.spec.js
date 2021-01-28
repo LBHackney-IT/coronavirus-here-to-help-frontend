9c116d6d5767c16ed653de072ac21fafe53f8846
Kat <katrina@madetech.com>
2021-01-28 11:44:10 +0000

    Extract some commands to before each for add-support tests Remove a test from reassign single call test Move some tests into journey folder

--- cypress/integration/journeys/add-support.spec.js
+++ cypress/integration/journeys/add-support.spec.js
@@ -1,13 +1,13 @@
 beforeEach(() => {
     cy.login();
-    cy.setIntercepts()
+    cy.setIntercepts();
     cy.visit(`http://localhost:3000/dashboard`);
+    cy.get('[data-testid=view-callback-list_button]').click();
+    cy.get('[data-testid=callbacks-list-view_link-0]').click({ force: true });
 });
-
+describe;
 context('When you view a helpcase profile', () => {
     it('it allows you to naviagte to the add support page', () => {
-        cy.visit(`http://localhost:3000/callback-list`);
-        cy.get('[data-testid=callbacks-list-view_link-0]').click({ force: true });
         cy.url().should('match', /\/helpcase-profile\/\d+$/);
         cy.get('[data-testid=add-support-button]').click({ force: true });
         cy.url().should('match', /\/add-support/);
@@ -16,8 +16,6 @@ context('When you view a helpcase profile', () => {
 
 context('When required fields are not filled in', () => {
     it('it displays validation error', () => {
-        cy.visit(`http://localhost:3000/callback-list`);
-        cy.get('[data-testid=callbacks-list-view_link-0]').click({ force: true });
         cy.get('[data-testid=add-support-button]').click({ force: true });
         cy.get('[data-testid=add-support-update_button]').click({ force: true });
         cy.get('[data-testid=add-support-validation-error]').should('be.visible');
@@ -34,8 +32,6 @@ context('When required fields are not filled in', () => {
 
 context('When required fields are filled in', () => {
     beforeEach(() => {
-        cy.visit(`http://localhost:3000/callback-list`);
-        cy.get('[data-testid=callbacks-list-view_link-0]').click({ force: true });
         cy.get('[data-testid=add-support-button]').click({ force: true });
         cy.get('[data-testid=call-type-radio-button]').first().click({ force: true });
         cy.get('[data-testid=call-type-no-radio-button]').click({ force: true });
@@ -51,8 +47,6 @@ context('When required fields are filled in', () => {
 
 context('When add support gets cancelled', () => {
     it('it returns to the resident page', () => {
-        cy.visit(`http://localhost:3000/callback-list`);
-        cy.get('[data-testid=callbacks-list-view_link-0]').click({ force: true });
         cy.get('[data-testid=add-support-button]').click({ force: true });
         cy.get('[data-testid=add-support-cancel_button]').click({ force: true });
         cy.get('[data-testid=add-support-validation-error]').should('not.exist');
