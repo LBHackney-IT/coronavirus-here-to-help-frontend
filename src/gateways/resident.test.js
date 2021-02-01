2057ec2e2457bef6e40681eee0a81a5f9c7050f3
Ben <ben.dalton@madetech.com>
2021-02-01 10:25:04 +0000

    Update first getResident test to check that axios calls with expected configuration

--- src/gateways/resident.test.js
+++ src/gateways/resident.test.js
@@ -12,59 +12,24 @@ describe('Resident gateway', () => {
         moxios.uninstall();
     });
 
-    it('gets the resident by id and correctly maps the response', async () => {
+    it('getResident method makes GET axios call to a correct url, with correct route parameter', async (done) => {
         // arrange
         const randomId = Math.floor(Math.random() * 20);
-        const mockAxiosResponse = 
-            {
-                Id: 1,
-                FirstName: "Russell",
-                LastName: "Botsford",
-                DobDay: "01",
-                DobMonth: "01",
-                DobYear: "1953",
-                ContactMobileNumber: "07084246866",
-                ContactTelephoneNumber: "02094644146",
-                EmailAddress: "Russell26@yahoo.com",
-                AddressFirstLine: "Flat 2",
-                AddressSecondLine: "9570 Gorczany Ford",
-                AddressThirdLine: "",
-                Postcode: "H6 3VA",
-                Uprn: "10007630135",
-                NhsNumber: "2106107057",
-            };
-
-        const expectedResponse = 
-            {
-                id: 1,
-                firstName: "Russell",
-                lastName: "Botsford",
-                dobDay: "01",
-                dobMonth: "01",
-                dobYear: "1953",
-                contactMobileNumber: "07084246866",
-                contactTelephoneNumber: "02094644146",
-                addressFirstLine: "Flat 2",
-                addressSecondLine: "9570 Gorczany Ford",
-                addressThirdLine: "",
-                emailAddress: "Russell26@yahoo.com",
-                postCode: "H6 3VA",
-                uprn: "10007630135",
-                nhsNumber: "2106107057",
-            };
-
         const urlExp = new RegExp('v4/residents/' + randomId);
-
-        moxios.stubRequest(urlExp, {
+        
+        moxios.stubRequest(/.+/, {
             status: 200,
-            response: mockAxiosResponse
+            response: [] 
         });
 
         // act
-        const gatewayResponse = await resGateway.getResident(randomId);
+        await resGateway.getResident(randomId);
 
         // assert
-        expect(gatewayResponse).toMatchObject(expectedResponse);
+        const request = moxios.requests.mostRecent();
+        expect(request.config.method).toEqual('get');
+        expect(request.url).toMatch(urlExp);
+        done();
     });
 });
 
