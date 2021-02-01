e57e775152b7a6de9f9d0e855052d1e054eaaafe
Ben <ben.dalton@madetech.com>
2021-02-01 11:09:20 +0000

    Extract residents mapping method into mapper class

--- src/mappers/inboundMapper.js
+++ src/mappers/inboundMapper.js
@@ -10,6 +10,26 @@ class InboundMapper {
             };
         });
     };
+
+    static ToResident = (response) => {
+        return {
+            id: response.Id,
+            firstName: response.FirstName,
+            lastName: response.LastName,
+            dobDay: response.DobDay,
+            dobMonth: response.DobMonth,
+            dobYear: response.DobYear,
+            contactTelephoneNumber: response.ContactTelephoneNumber,
+            contactMobileNumber: response.ContactMobileNumber,
+            emailAddress: response.EmailAddress,
+            addressFirstLine: response.AddressFirstLine,
+            addressSecondLine: response.AddressSecondLine,
+            addressThirdLine: response.AddressThirdLine,
+            postCode: response.Postcode,
+            uprn: response.Uprn,
+            nhsNumber: response.NhsNumber,
+        };
+    };
 }
 
 export default InboundMapper;
