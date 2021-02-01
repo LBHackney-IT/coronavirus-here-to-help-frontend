fbbe8db57d741b4169b8454f85e7f4f38446d1d9
Ben <ben.dalton@madetech.com>
2021-02-01 14:18:26 +0000

    Extract callback ToCallbackList mapping method to InboundMapper class

--- src/gateways/callback.js
+++ src/gateways/callback.js
@@ -1,35 +1,10 @@
 import { DefaultGateway } from '../gateways/default-gateway';
-import {CEV, SHIELDING} from '../helpers/constants';
-
-const isoDateToOtherDate = (dateString) => dateString.split('T')[0];
-const joinNameParts = (obj) => [obj.FirstName, obj.LastName].join(' ');
-const joinAddressParts = (obj) =>
-    [obj.AddressFirstLine, obj.AddressSecondLine, obj.AddressThirdLine].join(', ');
-const unsuccessfulCalls = (collection) =>
-    collection.filter((c) => /refused_to_engage|wrong_number/.test(c.CallOutcome)).length;
-const replaceIfShielding = (helpType) => helpType !== SHIELDING ? helpType : CEV;
-
-const ToCallbackList = (callbacks) => {
-    return callbacks?.map((callback) => {
-        return {
-            residentName: joinNameParts(callback),
-            residentId: callback.ResidentId,
-            helpRequestId: callback.Id,
-            address: joinAddressParts(callback),
-            requestedDate: callback.DateTimeRecorded ? isoDateToOtherDate(callback.DateTimeRecorded):"", //remove this line from the component
-            callType: replaceIfShielding(callback.HelpNeeded),
-            unsuccessfulCallAttempts: unsuccessfulCalls(callback.HelpRequestCalls),
-            followUpRequired: callback.CallbackRequired,
-            assignedTo: callback.AssignedTo,
-            rescheduledAt: callback.RescheduledAt
-        };
-    });
-};
+import InboundMapper from '../mappers/inboundMapper';
 
 export class CallbackGateway extends DefaultGateway {
     async getCallback(queryParams) {
         const response = await this.getFromUrl(`v3/help-requests/callbacks`);
-        const callbacksList = ToCallbackList(response);
+        const callbacksList = InboundMapper.ToCallbackList(response);
         return callbacksList;
     }
 }
--- src/mappers/inboundMapper.js
+++ src/mappers/inboundMapper.js
@@ -1,3 +1,13 @@
+import {CEV, SHIELDING} from '../helpers/constants';
+
+const isoDateToOtherDate = (dateString) => dateString.split('T')[0];
+const joinNameParts = (obj) => [obj.FirstName, obj.LastName].join(' ');
+const joinAddressParts = (obj) =>
+    [obj.AddressFirstLine, obj.AddressSecondLine, obj.AddressThirdLine].join(', ');
+const unsuccessfulCalls = (collection) =>
+    collection.filter((c) => /refused_to_engage|wrong_number/.test(c.CallOutcome)).length;
+const replaceIfShielding = (helpType) => helpType !== SHIELDING ? helpType : CEV;
+
 class InboundMapper {
     static ToCaseNotes = (caseNotes) => {
         return caseNotes?.map((note) => {
@@ -29,6 +39,23 @@ class InboundMapper {
             uprn: response.Uprn,
             nhsNumber: response.NhsNumber,
         };
+    };
+
+    static ToCallbackList = (callbacks) => {
+        return callbacks?.map((callback) => {
+            return {
+                residentName: joinNameParts(callback),
+                residentId: callback.ResidentId,
+                helpRequestId: callback.Id,
+                address: joinAddressParts(callback),
+                requestedDate: callback.DateTimeRecorded ? isoDateToOtherDate(callback.DateTimeRecorded):"", //remove this line from the component
+                callType: replaceIfShielding(callback.HelpNeeded),
+                unsuccessfulCallAttempts: unsuccessfulCalls(callback.HelpRequestCalls),
+                followUpRequired: callback.CallbackRequired,
+                assignedTo: callback.AssignedTo,
+                rescheduledAt: callback.RescheduledAt
+            };
+        });
     };
 }
 
