e924048299c034672d087d642a5e8f43ae5c416a
duslerke <liudvikast@gmail.com>
2021-02-01 02:38:44 +0000

    Added post case note gateway method unit test for testing whether axios gets called in an expected way

--- src/gateways/case-notes.test.js
+++ src/gateways/case-notes.test.js
@@ -1,6 +1,7 @@
 import moxios from 'moxios';
 import { CaseNotesGateway } from './case-notes.js';
 import { getMultipleCaseNotesV4_Speculative } from '../../tools/mockResponses';
+import { postSingleCaseNoteV4Body_Speculative } from '../../tools/mockRequestBodies';
 import InboundMapper from '../mappers/inboundMapper';
 
 describe('Case notes gateway', () => {
@@ -60,4 +61,37 @@ describe('Case notes gateway', () => {
         InboundMapper.ToCaseNotes = tempRealFunctionPointer;
         done(); //precautionary step to sure the code is sync (to avoid the side effects noise in between tests of modifying the global instances)
     });
+
+    it('postCaseNote method makes POST axios call to a correct url, with correct route parameters & body', async (done) => {
+        // arrange
+        const randomResidentId = Math.floor(Math.random() * 20);
+        const randomHelpRequestId = Math.floor(Math.random() * 20);
+        const mockDomainCaseNoteBody = postSingleCaseNoteV4Body_Speculative({
+            // non-essential set up now, but might become relevant if we do validation of path params against request body in the future
+            residentId: randomResidentId,
+            helpRequestId: randomHelpRequestId
+        });
+        moxios.stubRequest(/.+/, {
+            status: 200,
+            response: 0 // assuming endpoint only return's object id, though the value here is irrelevant
+        });
+        const urlExp = new RegExp(
+            `residents/${randomResidentId}/help-requests/${randomHelpRequestId}/caseNotes$`
+        );
+
+        // act
+        await csGateway.postCaseNote(randomResidentId, randomHelpRequestId, mockDomainCaseNoteBody);
+
+        // assert
+        let request = moxios.requests.mostRecent();
+        expect(request.config.method).toEqual('post');
+        expect(request.url).toMatch(urlExp); // tests url & route params at the same time
+        expect(request.config.data).toContain(mockDomainCaseNoteBody.caseNote); // essentially a check whether the request body exists or not upon axios call
+        // clean up
+        done();
+    });
+
+    // TODO: Add a test to check whether POST case note gateway method calls Domain -> API entity mapper.
+    // Currently it doesn't, so it's a good chance to implement that. This mapper would need to be under Outbound mappers
+    // file.
 });
