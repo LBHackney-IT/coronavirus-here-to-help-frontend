95e087329ac330487888ee51314b6e9eeb0139ea
Kat <katrina@madetech.com>
2021-02-02 12:35:42 +0000

    Uncomment some tests

--- cypress/integration/journeys/assing-calls.spec.js
+++ cypress/integration/journeys/assing-calls.spec.js
@@ -29,28 +29,28 @@ describe('As a call handler I can bulk assign calls', () => {
     //     });
     // });
 
-    // context('Assign call page displays and maps data correctly', () => {
-    //     it('Call handlers are retrieved and mapped to dropdown options', () => {
-    //         cy.get('[data-testid=assign-call-type_dropdown]')
-    //             .find('option')
-    //             .should('have.length', 5);
-    //         cy.get('[data-testid=assign-call-handler-checkbox]').should('have.length', 4);
-    //     });
-    // });
+    context('Assign call page displays and maps data correctly', () => {
+        it('Call handlers are retrieved and mapped to dropdown options', () => {
+            cy.get('[data-testid=assign-call-type_dropdown]')
+                .find('option')
+                .should('have.length', 5);
+            cy.get('[data-testid=assign-call-handler-checkbox]').should('have.length', 4);
+        });
+    });
 
-    // context('Assign call has validation', () => {
-    //     it('does not let you assign calls unless assignment type and handlers areselected', () => {
-    //         cy.get('[data-testid=assign-call-assign_button]').click({ force: true });
-    //         cy.get('[data-testid=assign-call-validation-error]').should('be.visible');
+    context('Assign call has validation', () => {
+        it('does not let you assign calls unless assignment type and handlers areselected', () => {
+            cy.get('[data-testid=assign-call-assign_button]').click({ force: true });
+            cy.get('[data-testid=assign-call-validation-error]').should('be.visible');
 
-    //         cy.get('[data-testid=assign-call-unassigned-checkbox]').click({ force: true });
-    //         cy.get('[data-testid=assign-call-assign_button]').click({ force: true });
-    //         cy.get('[data-testid=assign-call-validation-error]').should('be.visible');
+            cy.get('[data-testid=assign-call-unassigned-checkbox]').click({ force: true });
+            cy.get('[data-testid=assign-call-assign_button]').click({ force: true });
+            cy.get('[data-testid=assign-call-validation-error]').should('be.visible');
 
-    //         cy.get('[data-testid=assign-call-unassigned-checkbox]').click({ force: true });
-    //         cy.get('[data-testid=assign-call-handler-checkbox]').first().click({ force: true });
-    //         cy.get('[data-testid=assign-call-assign_button]').click({ force: true });
-    //         cy.get('[data-testid=assign-call-validation-error]').should('be.visible');
-    //     });
-    // });
+            cy.get('[data-testid=assign-call-unassigned-checkbox]').click({ force: true });
+            cy.get('[data-testid=assign-call-handler-checkbox]').first().click({ force: true });
+            cy.get('[data-testid=assign-call-assign_button]').click({ force: true });
+            cy.get('[data-testid=assign-call-validation-error]').should('be.visible');
+        });
+    });
 });
