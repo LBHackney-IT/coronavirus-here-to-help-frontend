907c2c0c8c01c09b8d25d67bf76aa7abc3a2b2a5
Kat <katrina@madetech.com>
2021-02-02 11:10:00 +0000

    Define the logic for assigning assigned and unassigned cases

--- src/pages/assign-calls.js
+++ src/pages/assign-calls.js
@@ -1,136 +1,209 @@
-import React from "react";
-import Layout from "../components/layout";
-import Link from "next/link";
+import React from 'react';
+import Layout from '../components/layout';
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
-import { Button, Checkbox, Dropdown } from "../components/Form";
+import { Button, Checkbox, Dropdown } from '../components/Form';
 import { CallHandlerGateway } from '../gateways/call-handler';
-import { CallbackGateway } from "../gateways/callback";
-import { HelpRequestGateway } from "../gateways/help-request";
+import { CallbackGateway } from '../gateways/callback';
+import { HelpRequestGateway } from '../gateways/help-request';
 import { useRouter } from 'next/router';
 
 export default function AssignCallsPage() {
     const router = useRouter();
     const [callHandlers, setCallHandlers] = useState([]);
     const [selectedCallHandlers, setSelectedCallHandlers] = useState([]);
-    const [selectedCallType, setSelectedCallType] = useState("All");
+    const [selectedCallType, setSelectedCallType] = useState('All');
+    const [selectedAssignment, setSelectedAssignment] = useState([]);
 
-    const callTypes = ["All", "Help Request", "CEV", "Welfare Call", "Contact Tracing"];
-
-    const getCallHandlers = async () => {
-      const gateway = new CallHandlerGateway();
-      const callHandlersList = await gateway.getCallHandler();
-
-      setCallHandlers(callHandlersList);
-  };
-
-  useEffect(getCallHandlers, []);
-
-  const updateSelectedCallHandlers = (value) => {
-    console.log(value)
-    if(selectedCallHandlers.includes(value)) {
-      let newSelectedCallHandlers = selectedCallHandlers.filter(callHandler => callHandler != value)
-      setSelectedCallHandlers(newSelectedCallHandlers)
-  }
-  else {
-      let newSelectedCallHandlers = selectedCallHandlers.concat([value])
-      setSelectedCallHandlers(newSelectedCallHandlers)
-  }
-  console.log(selectedCallHandlers)
-  }
-
-  const handleAssign = async (event) => {
+    const callTypes = ['All', 'Help Request', 'CEV', 'Welfare Call', 'Contact Tracing'];
 
     const callbackGateway = new CallbackGateway();
-    let callbacks = await callbackGateway.getCallback({});
     const gateway = new HelpRequestGateway();
 
-    if (selectedCallType!="All") {
-      callbacks = callbacks.filter(callback => callback.callType == selectedCallType)
+    const getCallHandlers = async () => {
+        const gateway = new CallHandlerGateway();
+        const callHandlersList = await gateway.getCallHandler();
+
+        setCallHandlers(callHandlersList);
+    };
+
+    useEffect(getCallHandlers, []);
+
+    const updateSelectedCallHandlers = (value) => {
+        if (selectedCallHandlers.includes(value)) {
+            let newSelectedCallHandlers = selectedCallHandlers.filter(
+                (callHandler) => callHandler != value
+            );
+            setSelectedCallHandlers(newSelectedCallHandlers);
+        } else {
+            let newSelectedCallHandlers = selectedCallHandlers.concat([value]);
+            setSelectedCallHandlers(newSelectedCallHandlers);
+        }
+    };
+
+    const assignCases = (cases, assignmentCount) => {
+      cases.forEach(async (callback) => {
+        const [sortedHandlers] = Object.entries(assignmentCount).sort(
+            ([, v1], [, v2]) => v1 - v2
+        );
+        assignmentCount[sortedHandlers[0]] += 1;
+
+        const updateObj = { assignedTo: sortedHandlers[0] };
+        await gateway.patchHelpRequest(callback.helpRequestId, updateObj);
+      });
+      return assignmentCount;
     }
 
-    let assignmentCount = {}
-    selectedCallHandlers.forEach(handler => assignmentCount = {...assignmentCount, [handler]: 0})
-
-    let unassignedCallbacks = [];
-    callbacks.forEach(callback => {
-      if(callback.assignedTo!=null){
-        if (selectedCallHandlers.includes(callback.assignedTo)){
-          console.log("callback.assignedTo")
-          console.log(callback.assignedTo)
-          console.log("selectedCallHandlers")
-          console.log(selectedCallHandlers)
-          assignmentCount[callback.assignedTo] += 1;
+    const handleAssign = async (event) => {
+        let callbacks = await callbackGateway.getCallback({});
+        if (selectedCallType != 'All') {
+            callbacks = callbacks.filter((callback) => callback.callType == selectedCallType);
         }
-        else {
-          unassignedCallbacks.push(callback)
+
+        let assignmentCount = {};
+        selectedCallHandlers.forEach(
+            (handler) => (assignmentCount = { ...assignmentCount, [handler]: 0 })
+        );
+
+        let unassignedCallbacks = [];
+        let assignedCallbacks = [];
+        callbacks.forEach((callback) => {
+          if (callback.assignedTo == null || !callback.assignedTo) {
+              unassignedCallbacks.push(callback);
+          } else {
+            assignedCallbacks.push(callback);
+          }
+         });
+
+        if (selectedAssignment == 'unassigned') {
+          assignCases(unassignedCallbacks, assignmentCount)
         }
+        else if(selectedAssignment == 'assigned') {
+          let toBeReassigned = [];
+          const averageCaseCount = assignedCallbacks.length / selectedCallHandlers.length
+          assignedCallbacks.forEach(callback => {
+            if (selectedCallHandlers.includes(callback.assignedTo) && assignmentCount[callback.assignedTo] < averageCaseCount){
+              assignmentCount[callback.assignedTo] += 1;
+            }
+            else {
+              toBeReassigned.push(callback)
+            }
+        });
+          assignCases(toBeReassigned, assignmentCount)
+        } else {
+          let toBeReassigned = [];
+          const averageCaseCount = callbacks.length / selectedCallHandlers.length
+          assignedCallbacks.forEach(callback => {
+            if (selectedCallHandlers.includes(callback.assignedTo) && assignmentCount[callback.assignedTo] < averageCaseCount){
+              assignmentCount[callback.assignedTo] += 1;
+            }
+            else {
+              toBeReassigned.push(callback)
+            }
+        });
+        let newAssignmentCount = assignCases(toBeReassigned, assignmentCount)
+        assignCases(unassignedCallbacks, newAssignmentCount)
       }
-      else{
-        unassignedCallbacks.push(callback)
-      }
-    });
 
-    unassignedCallbacks.forEach(async callback => {
-      const [sortedHandlers] = Object.entries(assignmentCount).sort(([ ,v1], [ ,v2]) => v1 - v2);
-      assignmentCount[sortedHandlers[0]] += 1;
+        router.push('/callback-list');
+    };
 
-      const updateObj = { assignedTo:  sortedHandlers[0] };
-      await gateway.patchHelpRequest(callback.helpRequestId, updateObj);
-    });
+    const updateSelectedAssignment = (value) => {
+        if (selectedAssignment.includes(value)) {
+            let newSelectedAssignment = selectedAssignment.filter(
+                (assignment) => assignment != value
+            );
+            setSelectedAssignment(newSelectedAssignment);
+        } else {
+            let newSelectedAssignment = selectedAssignment.concat([value]);
+            setSelectedAssignment(newSelectedAssignment);
+        }
+    };
 
-    router.push("/callback-list");
-}
-  return (
-    <Layout>
-      <div>
-        <Link href="/">
-            <a href="#" className="govuk-back-link  lbh-back-link">Back</a>
-        </Link>
-
-        <h1 className="govuk-heading-xl" style={{marginBottom: "20px"}}>Assign calls</h1>
-
-          <div className="govuk-!-margin-bottom-5">
-            <div className="govuk-grid-row">
-              <div className="govuk-grid-column-one-half">
-                <label className="govuk-label">Call types</label>
-                    <Dropdown dropdownItems={callTypes}    
-                              onChange={(type) => {setSelectedCallType(type);}}/>
-                <div className="govuk-hint">Select call help type</div>
-              </div>
-              <br />
-              <br />
-              <br />
-              <br />
-              <h3 className="govuk-heading-m" style={{marginRight: '1em', marginLeft: '.2em', marginTop: '2em'}}>Select who is able to make calls today</h3>
-
-              <div className="govuk-checkboxes  lbh-checkboxes">
-              
-              {callHandlers.map((callHandler) => {
-                    return (
-                        <Checkbox key={callHandler} label={callHandler} value={callHandler} onCheckboxChange={updateSelectedCallHandlers}/>
-                );
-              })}
-               
-                </div>
-                </div>
-          </div>
-
-          <div className="govuk-grid-row" id="btn-bottom-panel">
-            <div className="govuk-grid-column-one-half">
-            <Button
-                text="Assign"
-                addClass="govuk-!-margin-right-1"
-                onClick={(event)=> { handleAssign(event)}}
-            />             
-              <Link href="/">
-                    <Button
-                        text="Cancel"
-                        addClass="govuk-button--secondary"
-                    />
+    return (
+        <Layout>
+            <div>
+                <Link href="/">
+                    <a href="#" className="govuk-back-link  lbh-back-link">
+                        Back
+                    </a>
                 </Link>
+
+                <h1 className="govuk-heading-xl" style={{ marginBottom: '20px' }}>
+                    Assign calls
+                </h1>
+
+                <div className="govuk-!-margin-bottom-5">
+                    <div className="govuk-grid-row">
+                        <div className="govuk-grid-column-one-half">
+                            <label className="govuk-label">Call types</label>
+                            <Dropdown
+                                dropdownItems={callTypes}
+                                onChange={(type) => {
+                                    setSelectedCallType(type);
+                                }}
+                            />
+                            <div className="govuk-hint">Select call help type</div>
+                        </div>
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        <h3
+                            className="govuk-heading-m"
+                            style={{ marginRight: '1em', marginLeft: '.2em', marginTop: '2em' }}>
+                            Assign to
+                        </h3>
+                        <div className="govuk-checkboxes  lbh-checkboxes">
+                            <Checkbox
+                                key="unassigned-cases-checkbox"
+                                label="Unassigned cases"
+                                value="unassigned"
+                                onCheckboxChange={updateSelectedAssignment}
+                            />
+                            <Checkbox
+                                key="assigned-cases-checkbox"
+                                label="Assigned cases"
+                                value="assigned"
+                                onCheckboxChange={updateSelectedAssignment}
+                            />
+                        </div>
+                        <h3
+                            className="govuk-heading-m"
+                            style={{ marginRight: '1em', marginLeft: '.2em', marginTop: '2em' }}>
+                            Select who is able to make calls today
+                        </h3>
+
+                        <div className="govuk-checkboxes  lbh-checkboxes">
+                            {callHandlers.map((callHandler) => {
+                                return (
+                                    <Checkbox
+                                        key={callHandler}
+                                        label={callHandler}
+                                        value={callHandler}
+                                        onCheckboxChange={updateSelectedCallHandlers}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="govuk-grid-row" id="btn-bottom-panel">
+                    <div className="govuk-grid-column-one-half">
+                        <Button
+                            text="Assign"
+                            addClass="govuk-!-margin-right-1"
+                            onClick={(event) => {
+                                handleAssign(event);
+                            }}
+                        />
+                        <Link href="/">
+                            <Button text="Cancel" addClass="govuk-button--secondary" />
+                        </Link>
+                    </div>
+                </div>
             </div>
-          </div>
-      </div>
-    </Layout>
-  );
+        </Layout>
+    );
 }
