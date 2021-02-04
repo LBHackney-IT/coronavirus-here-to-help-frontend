1cd3b005c0a1361b8f9f70c79a8b5f6f714af7cc
Maysa <maysa@madetech.com>
2021-02-04 15:34:56 +0000

    fix edit page by handellling !casNotes

--- src/components/CaseNotes/CaseNotes.jsx
+++ src/components/CaseNotes/CaseNotes.jsx
@@ -6,7 +6,7 @@ import styles from '../CaseNotes/CaseNotes.module.scss';
 export default function CaseNotes({ caseNotes }) {
     const [filterBy, setFilterBy] = useState("")
     useEffect(()=>{
-        if(caseNotes.manageCaseNotePage){
+        if(caseNotes?.manageCaseNotePage){
             setFilterBy(caseNotes.helpNeeded)
         }else{
             setFilterBy("All")
@@ -18,31 +18,29 @@ export default function CaseNotes({ caseNotes }) {
     return (
         <div>
             <h2 className="govuk-heading-l">Case notes</h2>
-            { caseNotes[filterBy]?.length == 0 && 
+            {caseNotes && caseNotes[filterBy]?.length == 0 && 
                 <>
                     <div className ={ styles['case-notes-box']}>No previous case notes</div>
                     <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible" />
                 </>}
-                {caseNotes.All.length > 0 && <Dropdown  onChange={(e) => hanleOnChange(e)} dropdownItems ={helpTypes}></Dropdown>}
+                { caseNotes && caseNotes.All.length > 0 && <Dropdown  onChange={(e) => hanleOnChange(e)} dropdownItems ={helpTypes}></Dropdown>}
 
-            {caseNotes[filterBy]?.map((caseNote, i) => {
-                if(caseNote){
-                    return (
-                        <>
-                            <div
-                                key={i}
-                                id={`case-note-${i}`}
-                                className={`filter ${styles['case-notes-box']}`}
-                                data-testid="case-note-entry">
-                                <h4 className="govuk-heading-s">
-                                    {caseNote.formattedDate} by {caseNote.author}
-                                </h4>
-                                <p>{caseNote.helpNeeded}: {caseNote.note}</p>
-                            </div>
-                            <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible" />
-                        </>
-                    );
-                }
+            { caseNotes && caseNotes[filterBy]?.map((caseNote, i) => {
+                return (
+                    <>
+                        <div
+                            key={i}
+                            id={`case-note-${i}`}
+                            className={`filter ${styles['case-notes-box']}`}
+                            data-testid="case-note-entry">
+                            <h4 className="govuk-heading-s">
+                                {caseNote.formattedDate} by {caseNote.author}
+                            </h4>
+                            <p>{caseNote.helpNeeded}: {caseNote.note}</p>
+                        </div>
+                        <hr className="govuk-section-break govuk-section-break--m govuk-section-break--visible" />
+                    </>
+                );
             })}
         </div>
     );
