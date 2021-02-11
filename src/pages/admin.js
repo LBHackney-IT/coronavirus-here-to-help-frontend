ce7c5b0fa593ac091804fd6c4c3d654140078ccf
Marten Wetterberg <marten.wetterberg@nyweb.nu>
2021-02-11 16:41:44 +0000

    Create admin landing page

---
+++ src/pages/admin.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Layout from '../components/layout';
+import GovLinkBox from "../components/GovLinkBox/GovLinkBox";
+
+export default function AssignCallsPage() {
+    return (
+        <Layout>
+            <div>
+                <a
+                    href="/dashboard"
+                    className="govuk-back-link  lbh-back-link"
+                    data-testid="assign-call-back_button">
+                    Back
+                </a>
+
+                <h1 className="govuk-heading-xl" style={{ marginBottom: '20px' }}>
+                    Manager view
+                </h1>
+
+                <div className="govuk-grid-row" id="btn-bottom-panel">
+                    <GovLinkBox link="/assign-calls" text="Assign calls" name="assign-calls_button" />
+                </div>
+            </div>
+        </Layout>
+    );
+}
