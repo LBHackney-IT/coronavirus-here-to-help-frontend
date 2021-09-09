ce64e27b01bdda9a12ca56fb6765f0ebf3e58aa8
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2021-09-09 10:46:26 +0100

    Added in self isolation needs for contact tracing

--- cypress/integration/journeys/add-self-isolation-needs.spec.js
+++ cypress/integration/journeys/add-self-isolation-needs.spec.js
@@ -7,12 +7,11 @@ before(() => {
     cy.get('[data-testid=add-support-button]').click({ force: true });
 });
 
-
-context('When adding non self-isolation help request', () => {
+context('When adding non self-isolation or contact tracing help request', () => {
     it('it does not display the self-isolation needs form', () => {
         cy.get('[data-testid=self-isolation-needs]').should('not.exist');
 
-        cy.get('[data-testid=call-type-radio-button]').eq(0).click({ force: true });
+        cy.get('[data-testid=call-type-radio-button]').eq(1).click({ force: true });
         cy.get('[data-testid=call-type-yes-radio-button]').click({ force: true });
         cy.get('[data-testid=yes-spoke-to-resident]').click({ force: true });
         cy.get('[data-testid=callback_complete-checkbox]').click({ force: true });
@@ -35,6 +34,21 @@ context('When adding new self isolation help request and call completed is selec
     });
 });
 
+context('When adding new contact tracing help request and call completed is selected', () => {
+    it('displays self isolation help needs form', () => {
+        cy.get('[data-testid=callback_complete-checkbox]').click({ force: true });
+        cy.get('[data-testid=self-isolation-needs]').should('not.exist');
+
+        cy.get('[data-testid=call-type-radio-button]').eq(0).click({ force: true });
+        cy.get('[data-testid=call-type-yes-radio-button]').click({ force: true });
+        cy.get('[data-testid=yes-spoke-to-resident]').click({ force: true });
+        cy.get('[data-testid=self-isolation-needs]').should('not.exist');
+
+        cy.get('[data-testid=callback_complete-checkbox]').click({ force: true });
+        cy.get('[data-testid=self-isolation-needs]').should('be.visible');
+    });
+});
+
 context('When required fields are not filled in', () => {
     it('displays validation error', () => {
         cy.get('[data-testid=callback-form-update_button]').click({ force: true });
@@ -42,4 +56,3 @@ context('When required fields are not filled in', () => {
         cy.url().should('match', /\/add-support/);
     });
 });
-
