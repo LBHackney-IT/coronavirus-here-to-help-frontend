aa8006f20f70e0ce72107ffbb23e60dd57cda3c1
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2021-09-16 09:38:57 +0100

    Format tests

--- cypress/integration/journeys/send-bulk-message.spec.js
+++ cypress/integration/journeys/send-bulk-message.spec.js
@@ -1,18 +1,16 @@
 beforeEach(() => {
-  cy.login();
-  cy.setIntercepts();
-  cy.visit(`http://localhost:3000/dashboard`);
-
+    cy.login();
+    cy.setIntercepts();
+    cy.visit(`http://localhost:3000/dashboard`);
 });
 
 context('When adding new cev help request', () => {
-  it('it display cev help needs form', () => {
-    cy.get('[data-testid=admin_button]').click({force: true});
-    cy.get('[data-testid=send-bulk-message_button]').click({force: true});
-    cy.get('[data-testid=select-dropdown]').select("Contact Tracing");
-    cy.get('[data-testid=assigned-send-bulk-checkbox]').click({force: true});
-    cy.get('[data-testid=send-bulk-message_button]').click({force: true});
-    cy.url().should('contain', `dashboard`);
-  })
+    it('it display cev help needs form', () => {
+        cy.get('[data-testid=admin_button]').click({ force: true });
+        cy.get('[data-testid=send-bulk-message_button]').click({ force: true });
+        cy.get('[data-testid=select-dropdown]').select('Contact Tracing');
+        cy.get('[data-testid=assigned-send-bulk-checkbox]').click({ force: true });
+        cy.get('[data-testid=send-bulk-message_button]').click({ force: true });
+        cy.url().should('contain', `dashboard`);
+    });
 });
-
