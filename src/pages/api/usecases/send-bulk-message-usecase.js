d2514871dbeadc50958da25b6a6c3f06b7c48869
Owen Pigott <owen.pigott@madetech.com>
2021-09-22 14:07:11 +0100

    Formatting

--- src/pages/api/usecases/send-bulk-message-usecase.js
+++ src/pages/api/usecases/send-bulk-message-usecase.js
@@ -15,6 +15,7 @@ import { HereToHelpApiGateway } from '../../../gateways/here-to-help-api-gateway
 export class SendBulkMessagesUseCase {
     async sendMessages(reqBody) {
         const requestBody = JSON.parse(reqBody);
+
         try {
             const hereToHelpApiGateway = new HereToHelpApiGateway();
             const queryParam = requestBody.helpType == EUSS ? { IncludeType: 'EUSS' } : null;
