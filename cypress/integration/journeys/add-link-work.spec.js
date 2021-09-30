a74cd26a4271c4ebcef09e9e4e3654415a2cee1c
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2021-09-30 11:42:16 +0100

    Remove follow up email and text options for Link Work

--- cypress/integration/journeys/add-link-work.spec.js
+++ cypress/integration/journeys/add-link-work.spec.js
@@ -10,12 +10,14 @@ beforeEach(() => {
 });
 
 context('When adding new link work help request and call completed is selected', () => {
-    it('does not display self isolation help needs form and form can be submitted', () => {
+    it('does not display self isolation help needs form or follow up email or sms options and form can be submitted', () => {
         cy.get('[data-testid=call-type-radio-button]').eq(4).click({ force: true });
         cy.get('[data-testid=call-type-yes-radio-button]').click({ force: true });
         cy.get('[data-testid=yes-spoke-to-resident]').click({ force: true });
         cy.get('[data-testid=callback_complete-checkbox]').click({ force: true });
         cy.get('[data-testid=self-isolation-needs]').should('not.exist');
+        cy.get('[data-testid=send-text-checkbox]').should('not.exist');
+        cy.get('[data-testid=send-email-checkbox]').should('not.exist');
 
         cy.get('[data-testid=call-direction-radio-button]').first().click({ force: true });
         cy.get('[data-testid=followup-required-radio-button]').first().click({ force: true });
