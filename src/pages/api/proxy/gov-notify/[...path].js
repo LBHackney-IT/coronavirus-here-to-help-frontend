47b4f8dc4b7a5b96f32f10678bf0993a6471fc5b
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2021-10-13 16:11:39 +0100

    Information gathering: Is it always a buffer? When it's parsed, is it JSON? If I pass the parsed buffer, will this fix the problem?

--- src/pages/api/proxy/gov-notify/[...path].js
+++ src/pages/api/proxy/gov-notify/[...path].js
@@ -5,10 +5,21 @@ const sendMessageUseCase = new SendMessageUseCase();
 const endpoint = async (req, res) => {
     const user = authoriseUser(req);
     if (!user) return res.status(401).json({ error: 'Unauthorised' });
+    console.log("gov-notify root GW -------------------------------------------------------------------------")
+    console.log("Req query: ", req.query)
+    console.log(Buffer.isBuffer(req.body))
+    console.log(Object.getPrototypeOf(req.body))
+    console.log(req.body)
+
+    // parse from buffer to string
+    const requestBody = req.body.toString();
+    console.log("Parsed!!!!!!!!");
+    console.log(Buffer.isBuffer(requestBody));
+    console.log(requestBody);
 
     try {
         const { path, ...queryParams } = req.query;
-        const response = await sendMessageUseCase.sendMessage(path, queryParams, req.body);
+        const response = await sendMessageUseCase.sendMessage(path, queryParams, requestBody);
         res.json(response.data);
     } catch (error) {
         console.log('Error:' + error);
