87bcbedcbd2d29c4cde9fdb72930641f7553d3e6
Liudvikas Taluntis <43747286+Duslerke@users.noreply.github.com>
2021-10-13 17:23:03 +0100

    bulk message fix

--- src/pages/api/proxy/gov-notify/send-bulk-message.js
+++ src/pages/api/proxy/gov-notify/send-bulk-message.js
@@ -9,9 +9,26 @@ const endpoint = async (req, res) => {
     if (!user) return res.status(401).json({ error: 'Unauthorised' });
 
     try {
+        console.log("gov-notify root GW -------------------------------------------------------------------------")
+        console.log("Req query: ", req.query)
+        console.log(Buffer.isBuffer(req.body))
+        console.log(Object.getPrototypeOf(req.body))
+        console.log(req.body)
+    
+        // parse from buffer to string
+        const fromBuffer = req.body.toString();
+        console.log("Parsed!!!!!!!!");
+        console.log(Buffer.isBuffer(fromBuffer));
+        console.log(typeof fromBuffer);
+    
+        console.log("FROM JSON!!!!!!!!")
+        const parsedJSON = JSON.parse(fromBuffer);
+        console.log(typeof parsedJSON);
+        console.log(parsedJSON);
+
 
         await sendBulkMessagesUseCase.sendMessages(
-            req.body
+            parsedJSON
         );
         res.status(200).json({})
     } catch (err) {
