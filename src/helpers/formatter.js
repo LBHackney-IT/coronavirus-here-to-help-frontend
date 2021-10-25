3911b5cdf702af60679c497daa17fe0d63c23416
Owen Pigott <owen.pigott@madetech.com>
2021-10-25 13:56:29 +0100

    Removed unused method. Added new line.

--- src/helpers/formatter.js
+++ src/helpers/formatter.js
@@ -5,14 +5,3 @@ export const formatSubText = (text, subtext = '') => {
 export const formatSubTextMenu = (subtext = '') => {
     return subtext ? '--'.concat(subtext) : subtext;
 };
-
-export const splitName = (name) => {
-    if (!name) {
-        return { firstName: '', lastName: '' };
-    } else {
-        const splitName = name.split(' ');
-        const lastName = splitName.pop();
-        const firstName = splitName.join(' ');
-        return { firstName: firstName, lastName: lastName };
-    }
-};
