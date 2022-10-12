9496944251c3b06d2805c420f35798ff66278aab
duslerke <43747286+Duslerke@users.noreply.github.com>
2022-10-12 13:37:35 +0100

    Fiexed a couple of basic prettier formatting requirements.
    Though, I disabled the remaining warnings for the time being.

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Banner = ({ text }) => (
-    <div
-        className="govuk-notification-banner"
-        role="region"
-        aria-labelledby="govuk-notification-banner-title"
-        data-module="govuk-notification-banner">
+    <div className="govuk-notification-banner" role="region" aria-labelledby="govuk-notification-banner-title" data-module="govuk-notification-banner">
         <div className="govuk-notification-banner__header">
             <h2 className="govuk-notification-banner__title" id="govuk-notification-banner-title">
                 Important
